c0645d802d0b92403d47cca91967afd341840b3a
Valentin Robert <val@galois.com>
2020-01-17 11:44:24 -0800

    SAW shell.nix

---
+++ projects/saw/shell.nix
@@ -0,0 +1,28 @@
+{ nixpkgs ? import ./nixpkgs.nix }:
+with nixpkgs;
+mkShell {
+  buildInputs = [
+    cabal-install
+    ghc
+    hlint
+    nixpkgs.abc
+    #stack
+    zlib
+  ];
+  inputsFrom = [
+    #(import ./abcBridge.nix {}).env
+    #(import ./cryptol-verifier.nix {}).env
+  ];
+}
+
+
+# haskellPackages.shellFor {
+#   buildInputs = [
+#     (callPackage ./abc.nix {})
+#     stack
+#   ];
+#   name = "saw";
+#   packages = p: [
+#     p.abcBridge # need this so that libabc is present
+#   ];
+# }
