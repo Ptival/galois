69532d2de8d4be72e29bdc170579da114402c8c3
Valentin Robert <valentin.robert.42@gmail.com>
2020-02-10 20:04:09 +0100

    saw-core-coq: obtain PUMPKIN-PATCH via NUR

--- projects/saw/saw-script/deps/saw-core-coq/coq/shell.nix
+++ projects/saw/saw-script/deps/saw-core-coq/coq/shell.nix
@@ -1,12 +1,15 @@
-{ nixpkgs ? import <nixpkgs> {}
+{ nur ? (import <nixpkgs> {}).nur
+, nixpkgs ? import <nixpkgs> { overlays = [ nur.repos.ptival.overlays.PUMPKIN-PATCH ]; }
 }:
 with nixpkgs;
 mkShell {
   buildInputs = [
     coq
     coqPackages.coq-bits
+    coqPackages.coq-extensible-records
+    coqPackages.fix-to-elim
     coqPackages.mathcomp
-    (callPackage ./coq-extensible-records.nix {})
+    coqPackages.ornamental-search
   ];
   name = "saw-core-coq-coq";
 }
